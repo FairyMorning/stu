f0fe9416aedef7d76289d6b75b3d5a996f3b19f1
FairyMorning <44866090+FairyMorning@users.noreply.github.com>
2018-11-09 12:39:16 +0800

    Update and rename HelloController.java to HelloControl.java

--- controller/HelloControl.java
+++ controller/HelloControl.java
@@ -1,4 +1,4 @@
-package com.tedu.controller;
+package com.tedu.controller
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
